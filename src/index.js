e1528fdda06ecc4d7122b08c61d256f780b4c53a
Nicusor <lino479Nick@mail.ru>
2022-12-29 23:38:02 +0300

    router fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="https://chat-forntend.onrender.com">
       <App />
     </BrowserRouter>
   </React.StrictMode>
